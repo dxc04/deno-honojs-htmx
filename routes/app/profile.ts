c1b3a4b6a41fb5a8b393bb1a6b0efa2a8ea4fd2b
Dixie Philamerah Atay <dixie.atay@gmail.com>
2025-04-09 16:29:34 +0800

    Added alert feature

--- routes/app/profile.ts
+++ routes/app/profile.ts
@@ -1,7 +1,7 @@
 import { Context, Hono } from "hono";
 import { extractFormErrors, editProfileFormSchema, editPasswordFormSchema } from "../../lib/validators.ts";
 import { zValidator } from "@hono/zod-validator";
-import { renderSessionAppTemplate, renderFormPartialTemplate } from "../../lib/template.ts";
+import * as tmpl from "../../lib/template.ts";
 import * as UserTable from "../../db/model/users.ts";
 
 const app = new Hono();
@@ -10,7 +10,7 @@ app.get("/", async (c: Context) => {
   const session = c.get("session");
   const userId = session.get("userId");
   const user = await UserTable.findByUserId(userId);
-  const template = await renderSessionAppTemplate(user, "profile", { data: { first_name: user.first_name, last_name: user.last_name, email: user.email } });
+  const template = await tmpl.renderSessionApp(user, "profile", { data: { first_name: user.first_name, last_name: user.last_name, email: user.email } });
 
   c.res.headers.append("HX-Redirect", "/app/profile");
   
@@ -32,9 +32,11 @@ app.post(
         user.first_name = result.data.first_name;
         user.last_name = result.data.last_name;
         await UserTable.update(user);
+        
+        await tmpl.renderAlertSuccess("Profile updated successfully!", c.res.headers);
       }
 
-      const template = await renderFormPartialTemplate('edit_profile_form', { 
+      const template = await tmpl.renderFormPartial('edit_profile_form', { 
         data: {...result.data, email: formData.get("email")},
         errors
       });
@@ -57,9 +59,11 @@ app.post(
         const user = await UserTable.findUserByEmail(formData.get("email") as string);
         user.password = await UserTable.hashPassword(result.data.new_password);
         await UserTable.update(user);
+
+        await tmpl.renderAlertSuccess("User password updated successfully!", c.res.headers);
       }
 
-      const template = await renderFormPartialTemplate('edit_password_form', { 
+      const template = await tmpl.renderFormPartial('edit_password_form', { 
         data: {...result.data, email: formData.get("email")},
         errors
       });
